89944d1ed0dfc3d4e7af68bd7b5ad746319cbfc8
Tommi Ollila <138831117+TomG-FIN@users.noreply.github.com>
2023-07-20 05:45:51 +0300

    JS - Exercise 03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
 
+const forwardArray = inputString
+const reversedArray = [] 
+
+for (i = 1; i <= forwardArray.length; i++){
+    //console.log(forwardArray[i])
+    reversedArray[i] = forwardArray[forwardArray.length - i]
+}
+return reversedArray.join("")
 };
-
+console.log(reverseString("hello"))
 // Do not edit below this line
 module.exports = reverseString;
