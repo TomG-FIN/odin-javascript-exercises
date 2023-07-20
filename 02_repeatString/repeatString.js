baddcbe5b517884a8b3549b0fa32bb8c38717a19
Tommi Ollila <138831117+TomG-FIN@users.noreply.github.com>
2023-07-20 05:31:42 +0300

    JS - Exercise 02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(inputString, numberofTimes) {
 
+    const holderArray = []
+    if (typeof numberofTimes !== 'number') numberofTimes = 1; // Number sanity check
+    if (numberofTimes < 0) return "ERROR";
+    if (inputString === "") inputString = "";
+    for (i = 0; i < numberofTimes; i++){
+        holderArray.push(inputString);
+    }
+    console.log(holderArray)
+    return holderArray.join("")
 };
 
 // Do not edit below this line
