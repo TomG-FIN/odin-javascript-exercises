8d03861fd225e9529a8fc9c34e7194d281bf20f7
Tommi Ollila <138831117+TomG-FIN@users.noreply.github.com>
2023-07-25 01:31:25 +0300

    JS / Exercise 07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,30 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempToBeConverted) {
+
+  // Formula °F to °C
+  // x °F ≘ (x − 32) × 5/9 °C
+  
+  afterConversionNum = (tempToBeConverted - 32) * (5/9)
+  afterConversionNum = Math.round(afterConversionNum * 10) / 10
+  return afterConversionNum
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempToBeConverted) {
+
+//x °C ≘ (x × 9/5 + 32) °F
+afterConversionNum = (tempToBeConverted * (9/5) + 32)
+afterConversionNum = Math.round(afterConversionNum * 10) / 10
+return afterConversionNum
+
+
 };
+
+
+
+
+//console.log(convertToCelsius(32)) // fahrenheit to celsius, should return 0
+//console.log(convertToFahrenheit(0)) // celsius to fahrenheit, should return 32
+
+
 
 // Do not edit below this line
 module.exports = {
