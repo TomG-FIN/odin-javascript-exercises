c5da1cf14e60cbe91baf6f6c1af1be31b0832815
Tommi Ollila <138831117+TomG-FIN@users.noreply.github.com>
2023-07-25 01:11:52 +0300

    JS / Exercise 05 done. Had to add sanity checks before sum logic in order to pass tests. Solution had similar path to solution, but used isInteger instead of typeOf.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(firstNumberArg, lastNumberArg) {
+
+    if (firstNumberArg < 0 || lastNumberArg < 0) return "ERROR";
+    if (typeof firstNumberArg !== 'number') return "ERROR";
+    if (typeof lastNumberArg !== 'number' ) return "ERROR";
+
+    if (firstNumberArg > lastNumberArg) {
+        let switchVar = firstNumberArg;
+        firstNumberArg = lastNumberArg;
+        lastNumberArg = switchVar;
+    }
+
+    let totalSum = 0;
+
+    for (i = firstNumberArg; i <= lastNumberArg; i++ ) {
+        
+        totalSum += i;
+
+    }
+
+
+return totalSum;
 
 };
+
+//console.log(sumAll(1,4)) // returns the sum of 1 + 2 + 3 + 4 which is 10
 
 // Do not edit below this line
 module.exports = sumAll;
