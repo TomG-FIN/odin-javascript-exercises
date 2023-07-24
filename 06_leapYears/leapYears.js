44deb00801ededa6d4a1eda1698ed382f8fdfb5e
Tommi Ollila <138831117+TomG-FIN@users.noreply.github.com>
2023-07-25 01:21:44 +0300

    JS - Exercise 06 done. Similar path taken as solution, albeit more open.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+
+
+if (inputYear % 4 === 0){
+
+    if (inputYear % 100 === 0 && inputYear % 400 !== 0){
+        return false;
+    }
+
+    return true;
+}
+
+return false;
 
 };
+
+
+//console.log(leapYears(2000)) // True
+//console.log(leapYears(1985)) // is not a leap year: returns false
+//console.log(leapYears(1900)) // is not a leap year: returns false
 
 // Do not edit below this line
 module.exports = leapYears;
