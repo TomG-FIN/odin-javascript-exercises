8470782e30d1e8640e9f1dc5b3c8af31caec8b9e
Tommi Ollila <138831117+TomG-FIN@users.noreply.github.com>
2023-07-25 00:35:29 +0300

    JS - Exercise 04 done. A bit longer than expected. Finally found the solution using combination of Array methods (indexOf, filter and delete)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+// removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+
+const removeFromArray = function(inputArray, ...removableList) {
+
+    let noOfArgsToRemove = removableList.length;
+    let finalArray = [];
+
+    for (let i = 0; i < noOfArgsToRemove; i++){
+
+        let toBeRemovedItemIndex = inputArray.indexOf(removableList[i]);
+            delete inputArray[toBeRemovedItemIndex];
+
+    }
+
+    inputArray = inputArray.filter( Boolean );
+    return inputArray;
 
 };
 
